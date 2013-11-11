69066dee6729ce0291261e992d05e9521c6273db
Peter Collins <pmcollins2@wisc.edu>
2013-11-10 19:57:18 -0800

    Hey performance testing finally works

--- bipartite/bipartite_topo.py
+++ bipartite/bipartite_topo.py
@@ -1,14 +1,18 @@
 #!/usr/bin/python
 
 from mininet.topo import Topo
-from mininet.node import CPULimitedHost, Controller
+from mininet.node import CPULimitedHost
 from mininet.link import TCLink
 from mininet.net import Mininet
-from mininet.log import lg, info
-from mininet.util import dumpNodeConnections
+from mininet.log import output, debug
+from mininet.util import dumpNetConnections
 from mininet.cli import CLI
 
+import os
+import re
+import signal
 from argparse import ArgumentParser
+from time import sleep
 
 class BipartiteTopo(Topo):
     # Bipartite topology
@@ -31,6 +35,57 @@ class BipartiteTopo(Topo):
         for i in range(n):
             hosts.append(self.addHost('h'+str(i+1)))
             self.addLink(switches[i/2], hosts[i])
+
+def dumpNodeAddresses( nodes ):
+    "Dump addresses to/from nodes."
+
+    for node in nodes:
+        output("Host "+node.name+" has IP address "+node.IP()+" and MAC address "+node.MAC()+"\n")
+
+def dumpNetAddresses( net ):
+    "Dump addresses in network"
+    nodes = net.controllers + net.switches + net.hosts
+    dumpNodeAddresses( nodes )
+
+def _parsePerf( perfOutput ):
+    """Parse perf output and return bandwidth.
+       perfOutput: string
+       returns: result string"""
+    r = r'([\d\.]+ \w+/sec)'
+    m = re.findall( r, perfOutput )
+    if m:
+        return m[-1]
+    else:
+        # was: raise Exception(...)
+        error( 'could not parse perf output: ' + perfOutput )
+        return ''
+
+
+def perf( net, hosts=None ):
+    """Performance between all specified hosts.
+       hosts: list of hosts"""
+    if not hosts:
+        hosts = net.hosts
+        output( '*** Perf: testing performance\n' )
+    for node in hosts:
+        output( '%s -> ' % node.name )
+        node.cmd( 'killall -9 iperf' )
+        node.sendCmd('./iperf.sh s', printPid=True)
+        nodeLastPid = 0
+        while node.lastPid is None:
+            node.monitor()
+        nodeLastPid = node.lastPid
+        for dest in hosts:
+            if node != dest:
+                while 'Connected' not in dest.cmd('sh -c "echo A | telnet -e A %s 5001"' % node.IP()):
+                    #output( 'waiting %s %s\n' % (node.name, dest.name ))
+                    sleep(.5)
+                result = dest.cmd('./iperf.sh c %s' % (node.IP()) )
+                output(('%s: %s ' % (dest.name,_parsePerf(result))) if result else 'X ' )
+        output( '\n' )
+        os.kill(nodeLastPid, signal.SIGKILL)
+        node.waitOutput()
+        node.cmd( 'killall -9 iperf' )
 
 def test():
     # Parse arguments
@@ -73,9 +128,14 @@ def test():
 
     topo = BipartiteTopo(n=args.half_switches*4, bw_net=args.bandwidth, delay='%sms' % args.delay, maxq=args.max_queue, loss=None)
     net = Mininet(topo=topo, link=TCLink, host=CPULimitedHost)
+    #for switch in net.switches:
+    #    switch.setIP('10.1.0.'+str(net.switches.index(switch)+1))
     net.start()
-    dumpNodeConnections(net.hosts)
-    dumpNodeConnections(net.switches)
+    dumpNetConnections(net)
+    dumpNodeAddresses(net.hosts)
+    #dumpNodeAddresses(net.switches)
+    net.pingAll()
+    perf(net)
     CLI(net)
     net.stop()
     
